00c14026218f47d54df1ca40df5a7946dc9eee17
Oksana0709 <o07091980@gmail.com>
2023-04-23 12:55:56 +0300

    [сделала коментарииt]

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,5 +6,5 @@ const Button = ({fullWidth, text}) => {
         <button style={{width: fullWidth ? '100%' : 'fit-content'}} className="button">{text ? text : 'Click!'}</button>
     );
 };
-
+//*****
 export default Button
