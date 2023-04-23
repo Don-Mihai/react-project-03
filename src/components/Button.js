5d26f0fd3af997bc55dcbaa4498b75317311e376
Palienko Mihai <palienko@advantum.pro>
2023-04-23 11:11:10 +0300

    [feat] initialize project []

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Button.scss'
+
+const Button = ({fullWidth, text}) => {
+    return (
+        <button style={{width: fullWidth ? '100%' : 'fit-content'}} className="button">{text ? text : 'Click!'}</button>
+    );
+};
+
+export default Button
