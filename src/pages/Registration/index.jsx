86bb0a7aa1e0308e82fe95cf2e98cb9b600047c0
Palienko Mihai <palienko@advantum.pro>
2023-04-29 12:34:20 +0300

    [feat] material []

--- src/pages/Registration/index.jsx
+++ src/pages/Registration/index.jsx
@@ -1,10 +1,11 @@
 import './Registration.scss';
+import TextField from '@mui/material/TextField';
 
 const Registration = () => {
     return (
         <div className="page-registration">
             <h2>Фриланс</h2>
-            <input type="text" />
+            <TextField id="filled-basic" label="Filled" variant="filled" />
         </div>
     );
 };
