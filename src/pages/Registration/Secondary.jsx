ab8318471d171c766129f708d84331892c068453
Milena <mi_lky@mail.ru>
2023-05-05 15:16:01 +0300

    [feat] add user status field to registration form

--- src/pages/Registration/Secondary.jsx
+++ src/pages/Registration/Secondary.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import './Registration.scss';
 import TextField from '@mui/material/TextField';
+import ProfileType from './ProfileType';
 
-const Secondary = ({}) => {
+const Secondary = ({ onChange, formValues }) => {
     return (
         <>
             <TextField label="Имя" name="name" variant="filled" fullWidth />
             <TextField label="Фамилия" name="surname" variant="filled" fullWidth />
+            <ProfileType formValues={formValues} onChange={onChange} />
         </>
     );
 };
