469d6cea725674f67722cdb3615d8ec694179d56
weeRGNo136 <dchtwefy246940@vk.com>
2023-05-06 11:17:47 +0300

    [addUptade]

--- src/pages/Registration/Secondary.jsx
+++ src/pages/Registration/Secondary.jsx
@@ -2,11 +2,11 @@ import { useState } from 'react';
 import './Registration.scss';
 import TextField from '@mui/material/TextField';
 
-const Secondary = ({}) => {
+const Secondary = ({ formValues, onChange }) => {
     return (
         <>
-            <TextField label="Имя" name="name" variant="filled" fullWidth />
-            <TextField label="Фамилия" name="surname" variant="filled" fullWidth />
+            <TextField onChange={onChange} value={formValues.name} name="name" label="имя" variant="filled" fullWidth />
+            <TextField onChange={onChange} value={formValues.surname} name="surname" label="фамилия" variant="filled" fullWidth />
         </>
     );
 };
