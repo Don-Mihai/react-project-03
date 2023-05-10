d042f72e2844ebcbf83e08d19e4d48a82a40d4c7
Nikita Argunov <chichiletme@gmail.com>
2023-05-10 12:16:13 +0300

    [fix] add orders []

--- src/components/FreelanceSection/index.tsx
+++ src/components/FreelanceSection/index.tsx
@@ -11,6 +11,20 @@ import Stack from '@mui/material/Stack';
 
 const cn = bemCreator('freelance-section');
 
+export interface Orders {
+    id: number;
+    name: string;
+    skills: string[];
+    price: number;
+}
+
+export const orders = [
+    { id: 1, name: 'Разработка интернет-магазина', skills: ['React', 'Js', 'CSS'], price: 10000 },
+    { id: 2, name: 'Дизайн для мобильного приложения', skills: ['Fimga', 'AdobePhotoShop', 'CSS'], price: 11000 },
+    { id: 3, name: 'Обучение нейросети', skills: ['Python', 'DataScience', 'SQL', 'Apache'], price: 12000 },
+    { id: 4, name: 'Разработка мобильного приложения', skills: ['ReactNative', 'Typescript', '.Net'], price: 13000 },
+];
+
 const FreelanceSection = ({}) => {
     return (
         <section className={cn()}>
@@ -19,24 +33,28 @@ const FreelanceSection = ({}) => {
                 <Button variant="outlined">Все фриланс-проекты</Button>
             </div>
             <div className={cn('order-container')}>
-                <Card className={cn('order')}>
-                    <h3 className={cn('order-title')}>Разработка интернет-магазина</h3>
-                    <Stack direction="row" spacing={1}>
-                        <Chip label="React" />
-                        <Chip label="Js" />
-                        <Chip label="CSS" />
-                    </Stack>
-                    <hr className={cn('line')} />
-                    <CardActions className={cn('button-contaner')}>
-                        <div>
-                            Стоимость: <br />
-                            10000р.
-                        </div>
-                        <Button className={cn('button')} variant="contained" size="small">
-                            Подробнее
-                        </Button>
-                    </CardActions>
-                </Card>
+                {orders.map(order => {
+                    return (
+                        <Card key={order.id} className={cn('order')}>
+                            <h3 className={cn('order-title')}>{order.name}</h3>
+                            <Stack direction="row" spacing={1}>
+                                {order.skills.map((skill, index) => {
+                                    return <Chip key={index} label={skill} />;
+                                })}
+                            </Stack>
+                            <hr className={cn('line')} />
+                            <CardActions className={cn('button-contaner')}>
+                                <div>
+                                    Стоимость: <br />
+                                    {order.price}р.
+                                </div>
+                                <Button className={cn('button')} variant="contained" size="small">
+                                    Подробнее
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    );
+                })}
             </div>
         </section>
     );
