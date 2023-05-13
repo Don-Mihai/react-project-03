4b5a2cb7cb04d0410034c82b519100729a549a43
Palienko Mihai <palienko@advantum.pro>
2023-05-13 11:24:25 +0300

    [feat] unite []

--- src/pages/HomeNew/index.tsx
+++ src/pages/HomeNew/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from '../../components/Hero';
 import { Freelancer } from '../../App';
 import CustomersSection from '../../components/CustomersSection';
+import FreelanceSection from '../../components/FreelanceSection';
 
 interface Props {
     freelancers: Freelancer[];
@@ -12,6 +13,7 @@ const Home = ({ freelancers }: Props) => {
         <>
             <Hero />
             <CustomersSection sectionTitle="Раздел заказчиков" buttonText="Все фрилансеры" freelancers={freelancers} />
+            <FreelanceSection />
         </>
     );
 };
