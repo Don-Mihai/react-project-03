a877f72b980402946aa5f90c025364142404834e
Palienko Mihai <palienko@advantum.pro>
2023-06-24 11:05:29 +0300

    [feat] mongoDb []

--- server/index.js
+++ server/index.js
@@ -1,12 +1,31 @@
 const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const conectDb = require('./config/bd');
+const Schema = mongoose.Schema;
+// const expressAsyncHandler = require('express-async-handler');
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.get('/users', function (request, response) {
-    // отправляем ответ
-    response.send(['user1', 'user2', 'user3']);
+conectDb();
+
+// установка схемы
+const userScheme = new Schema({
+    name: String,
+    age: Number,
+});
+
+const User = mongoose.model('User', userScheme);
+
+app.post('/create-user', async function (request, response) {
+    const data = request.body;
+
+    const user = await User.create(data);
+    console.log(data);
+
+    response.send(user).status(200);
 });
 
 app.listen(PORT, () => {
